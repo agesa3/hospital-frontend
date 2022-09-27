6bcfe94373248998e4f2e61e129fd409f951ad09
agesa3 <collinsagesa2000@gmail.com>
2022-09-27 21:53:03 +0300

    code cleanup

--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
 //API URL ENDPOINT
-export const API_URL = 'http://139.144.25.153:9001/patients/';
-// export const API_URL = 'http://localhost:9001/patients/';
+export const API_URL = 'http://139.144.25.153:9001/patients';
+// export const API_URL = 'http://localhost:9001/patients';
